5aa2353a90cbdf23570154becd3306d2b16e44b2
Pradeep Kumar <pradeep@appsphere.in>
2020-04-02 15:00:55 +0530

    otp timer, state search

--- apps/frontend/src/app/login/login.component.ts
+++ apps/frontend/src/app/login/login.component.ts
@@ -4,16 +4,17 @@ import { AuthService } from '../shared/services/auth.service';
 import { AuthQuery } from '../queries/auth/auth.query';
 import { Router } from '@angular/router';
 import { untilDestroyed } from 'ngx-take-until-destroy';
-import { AuthService as SocialAuthService, GoogleLoginProvider } from 'angularx-social-login';
+import {
+  AuthService as SocialAuthService,
+  GoogleLoginProvider
+} from 'angularx-social-login';
 import { NzNotificationService } from 'ng-zorro-antd';
 import { User } from '@covid19-helpline/models';
-
 
 @Component({
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.scss']
 })
-
 export class LoginComponent implements OnInit, OnDestroy {
   public loginForm: FormGroup;
   public otpForm: FormGroup;
@@ -22,15 +23,20 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   public verificationInProcess: boolean;
   public resendInProcess: boolean;
+  public resendInProcessWaitTime: number;
+
   public loginInProcess: boolean;
   public isSubmitted: boolean;
   public isLoginSuccess: boolean;
+  public interval: any;
 
-  constructor(private _authService: AuthService,
-              private _authQuery: AuthQuery,
-              private router: Router,
-              protected notification: NzNotificationService,
-              private socialAuthService: SocialAuthService) {
+  constructor(
+    private _authService: AuthService,
+    private _authQuery: AuthQuery,
+    private router: Router,
+    protected notification: NzNotificationService,
+    private socialAuthService: SocialAuthService
+  ) {
     this.notification.config({
       nzPlacement: 'bottomRight'
     });
@@ -42,27 +48,44 @@ export class LoginComponent implements OnInit, OnDestroy {
     });
 
     this.otpForm = new FormGroup({
-      mobileNumber : new FormControl(null, [Validators.required]),
+      mobileNumber: new FormControl(null, [Validators.required]),
       code: new FormControl(null, [Validators.required])
     });
 
-    this._authQuery.isLoginInProcess$.pipe(untilDestroyed(this)).subscribe(res => {
-      this.loginInProcess = res;
-    });
+    this._authQuery.isLoginInProcess$
+      .pipe(untilDestroyed(this))
+      .subscribe(res => {
+        this.loginInProcess = res;
+      });
 
-    this._authQuery.isVerificationInProcess$.pipe(untilDestroyed(this)).subscribe(res => {
-      this.verificationInProcess = res;
-    });
+    this._authQuery.isVerificationInProcess$
+      .pipe(untilDestroyed(this))
+      .subscribe(res => {
+        this.verificationInProcess = res;
+      });
 
-    this._authQuery.isResendInProcess$.pipe(untilDestroyed(this)).subscribe(res => {
-      this.resendInProcess = res;
-    });
+    this._authQuery.isResendInProcess$
+      .pipe(untilDestroyed(this))
+      .subscribe(res => {
+        this.resendInProcess = res;
+      });
 
-    this._authQuery.isLoginSuccess$.pipe(untilDestroyed(this)).subscribe(res => {
-      if (this.isSubmitted && res) {
-        this.isLoginSuccess = true;
-      }
-    });
+    this._authQuery.isResendInSuccess$
+      .pipe(untilDestroyed(this))
+      .subscribe(res => {
+        if (res) {
+          this.startTimer(60);
+        }
+      });
+
+    this._authQuery.isLoginSuccess$
+      .pipe(untilDestroyed(this))
+      .subscribe(res => {
+        if (this.isSubmitted && res) {
+          this.isLoginSuccess = true;
+          this.startTimer(60);
+        }
+      });
 
     // auth state subscriber if user and user token found then verify that token and re-login user
     // this.socialAuthService.authState.pipe(untilDestroyed(this)).subscribe((user) => {
@@ -70,7 +93,6 @@ export class LoginComponent implements OnInit, OnDestroy {
     //     this._authService.googleSignIn(user.idToken).subscribe();
     //   }
     // });
-
   }
 
   // loginWithGoogle() {
@@ -81,50 +103,66 @@ export class LoginComponent implements OnInit, OnDestroy {
   // }
 
   submitForm() {
-    try{
-
+    try {
       this.isSubmitted = true;
 
-      this.otpForm.get('mobileNumber').patchValue(this.loginForm.value.mobileNumber);
+      this.otpForm
+        .get('mobileNumber')
+        .patchValue(this.loginForm.value.mobileNumber);
 
       this._authService.login(this.loginForm.value).subscribe();
-
-    }catch (e) {
+    } catch (e) {
       this.isSubmitted = false;
     }
-
   }
 
   submitOTP() {
-
     try {
-
       this.isSubmitted = true;
 
       this._authService.verifyOtp(this.otpForm.value).subscribe();
-
-    }catch (e) {
+    } catch (e) {
       this.isSubmitted = false;
     }
-
   }
-
 
   public resendOtp() {
     try {
-      this.otpSendingInProcess =true;
+      this.otpSendingInProcess = true;
 
-      const json : User = {
+      const json: User = {
         mobileNumber: this.otpForm.get('mobileNumber').value
-      }
+      };
 
       this._authService.resendOtp(json).subscribe();
-
-    }catch (e) {
-      this.otpSendingInProcess =false;
+    } catch (e) {
+      this.otpSendingInProcess = false;
     }
   }
 
-  ngOnDestroy(): void {
+  // if otp fields start filling the sms message will hide
+  public checkField() {
+    if (this.otpForm.get('code').value.length > 0) {
+      this.resendInProcessWaitTime = 0;
+      console.log('clearInterval/stopped: ', this.resendInProcessWaitTime);
+      clearInterval(this.interval); // stop interval tick
+    }
   }
+
+  public startTimer(durationInSec: number) {
+    this.resendInProcessWaitTime = durationInSec;
+
+    this.interval = setInterval(() => {
+      console.log(this.resendInProcessWaitTime);
+
+      this.resendInProcessWaitTime--;
+
+      if (this.resendInProcessWaitTime === 0) {
+        console.log('clearInterval/stopped: ', this.resendInProcessWaitTime);
+        clearInterval(this.interval); // stop interval tick
+      }
+    }, 1000);
+  }
+
+  ngOnDestroy(): void {}
 }
