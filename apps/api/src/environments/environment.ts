29f7db58b0202a56592dbfbd59b5e6b6ea4a10f1
Pradeep Kumar <pradeep@appsphere.in>
2020-04-13 10:49:12 +0530

    API_URL

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   APP_URL: 'http://localhost:4200/',
-  APP_URL: 'http://localhost:3333/api/',
+  API_URL: 'http://localhost:3333/api/',
   //API_URL: 'http://192.168.0.105:3333/api/', pradeep pc
 };
