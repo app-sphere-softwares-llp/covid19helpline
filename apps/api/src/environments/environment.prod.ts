53ef4f0cc32212f881b871d75bca46c608da9c29
Pradeep Kumar <pradeep@appsphere.in>
2020-04-14 20:30:24 +0530

    ejs font issue fixed

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  APP_URL: 'https://139.162.49.179:4200/',
+  APP_URL: 'http://139.162.49.179:4200/',
   API_URL: 'http://139.162.49.179:3333/api/',
 };
